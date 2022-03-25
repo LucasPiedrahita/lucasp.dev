ca2f0bb8afc8dd2790a3f72621dbaab7f2d7c23f
LucasPiedrahita <Lucas.Piedrahita96@gmail.com>
2022-03-24 21:30:01 -0400

    add social links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react'
-import { Grid, Avatar, IconButton, Drawer } from '@mui/material'
+import { Grid, Avatar, IconButton, Drawer, useTheme, useMediaQuery } from '@mui/material'
 import { Menu } from '@mui/icons-material'
 
 import avatarImage from '../../assets/lucas-piedrahita-196.jpeg'
@@ -7,7 +7,10 @@ import { SocialLinks } from '../'
 
 const styles = {
   container: {
-    p: 2,
+    p: {
+      xs: 2,
+      sm: 3,
+    },
   },
   avatar: {
     width: 52,
@@ -18,18 +21,24 @@ const styles = {
 function Navbar() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   const toggleDrawer = useCallback(() => setIsDrawerOpen(!isDrawerOpen), [isDrawerOpen])
+  const theme = useTheme()
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('sm'))
+
   return (
-    <>
-      <Grid container justifyContent='space-between' alignItems='center' sx={styles.container}>
-        <Avatar alt='Lucas Piedrahita' src={avatarImage} sx={styles.avatar} />
-        <IconButton onClick={toggleDrawer} size='large' aria-label='open navigation menu'>
-          <Menu fontSize='inherit' />
-        </IconButton>
-      </Grid>
-      <Drawer anchor='bottom' open={isDrawerOpen} onClose={toggleDrawer}>
-        <SocialLinks />
-      </Drawer>
-    </>
+    <Grid container justifyContent='space-between' alignItems='center' sx={styles.container}>
+      <Avatar alt='Lucas Piedrahita' src={avatarImage} sx={styles.avatar} />
+      {isLargeScreen && <SocialLinks horizontal={isLargeScreen} />}
+      {!isLargeScreen && (
+        <>
+          <IconButton onClick={toggleDrawer} size='large' aria-label='open navigation menu'>
+            <Menu fontSize='inherit' />
+          </IconButton>
+          <Drawer anchor='bottom' open={isDrawerOpen} onClose={toggleDrawer}>
+            <SocialLinks />
+          </Drawer>
+        </>
+      )}
+    </Grid>
   )
 }
 
--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import {
   GitHub,
@@ -8,66 +9,111 @@ import {
 } from '@mui/icons-material'
 import resume from '../../assets/Piedrahita_Lucas_Resume.pdf'
 
-const styles = {
+const verticalStyles = {
   container: {
     width: '100%',
     bgcolor: 'background.paper',
   },
-  avatar: {
-    width: 52,
-    height: 52,
-  },
+  resumeLink: {},
 }
 
-function Navbar() {
+const horizontalStyles = {
+  container: {
+    ...verticalStyles.container,
+    width: 'auto',
+    '& ul': {
+      p: 0,
+      display: 'flex',
+      gap: 3,
+    },
+    '& .MuiListItemIcon-root': {
+      minWidth: 0,
+      '& svg': {
+        width: '1.75rem',
+        height: '1.75rem',
+      },
+    },
+  },
+  resumeLink: {
+    color: 'primary.main',
+    border: 2,
+    borderRadius: 2,
+    borderColor: 'primary.main',
+    px: 1.5,
+    py: 0.5,
+  },
+}
+SocialLinks.propTypes = {
+  horizontal: PropTypes.bool,
+}
+function SocialLinks({ horizontal = false }) {
+  const styles = horizontal ? horizontalStyles : verticalStyles
   return (
-    <Box sx={styles.container}>
-      <nav aria-label='contact information'>
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton component='a' href='https://www.linkedin.com/in/lucas-piedrahita/'>
-              <ListItemIcon>
-                <LinkedIn />
-              </ListItemIcon>
-              <ListItemText primary='LinkedIn' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component='a' href='https://github.com/LucasPiedrahita'>
-              <ListItemIcon>
-                <GitHub />
-              </ListItemIcon>
-              <ListItemText primary='GitHub' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component='a' href='mailto:lpiedrahita96@gmail.com'>
-              <ListItemIcon>
-                <MailOutline />
-              </ListItemIcon>
-              <ListItemText primary='Email' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component='a' href='tel:+19192745369'>
-              <ListItemIcon>
-                <PhoneIphone />
-              </ListItemIcon>
-              <ListItemText primary='Call' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component='a' href={resume} target='_blank'>
+    <Box sx={styles.container} component='nav' aria-label='contact information'>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton
+            component='a'
+            href='https://www.linkedin.com/in/lucas-piedrahita/'
+            disableGutters={horizontal}
+          >
+            <ListItemIcon>
+              <LinkedIn />
+            </ListItemIcon>
+            {!horizontal && <ListItemText primary='LinkedIn' />}
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton
+            component='a'
+            href='https://github.com/LucasPiedrahita'
+            disableGutters={horizontal}
+          >
+            <ListItemIcon>
+              <GitHub />
+            </ListItemIcon>
+            {!horizontal && <ListItemText primary='GitHub' />}
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton
+            component='a'
+            href='mailto:lpiedrahita96@gmail.com'
+            disableGutters={horizontal}
+          >
+            <ListItemIcon>
+              <MailOutline />
+            </ListItemIcon>
+            {!horizontal && <ListItemText primary='Email' />}
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='tel:+19192745369' disableGutters={horizontal}>
+            <ListItemIcon>
+              <PhoneIphone />
+            </ListItemIcon>
+            {!horizontal && <ListItemText primary='Call' />}
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton
+            component='a'
+            href={resume}
+            target='_blank'
+            disableGutters={horizontal}
+            sx={styles.resumeLink}
+          >
+            {!horizontal && (
               <ListItemIcon>
                 <ContactPageOutlined />
               </ListItemIcon>
-              <ListItemText primary='Resume' />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
+            )}
+            <ListItemText primary='Resume' />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   )
 }
 
-export default Navbar
+export default SocialLinks
