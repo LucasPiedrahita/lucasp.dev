597e1259a7af75a0a0bc9692ef28e0a7bb0a71a8
LucasPiedrahita <Lucas.Piedrahita96@gmail.com>
2022-07-15 16:00:21 -0400

    Add react-snap for pre-rendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import { CssBaseline, GlobalStyles } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 
@@ -79,16 +79,24 @@ const globalStyles = (
   />
 )
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {globalStyles}
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+function WrappedApp() {
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {globalStyles}
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  )
+}
+
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(<WrappedApp />, rootElement)
+} else {
+  render(<WrappedApp />, rootElement)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
